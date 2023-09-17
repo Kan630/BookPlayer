bb9a6e42522af00ce7c7a711a18d775bcc474723
Antoine Driot <adriot@gmail.com>
2023-09-18 00:12:47 +0200

    DB v3 (migration) avec new col displayName => ok

--- app/src/main/java/com/driot/bookplayer/db/AppDatabase.java
+++ app/src/main/java/com/driot/bookplayer/db/AppDatabase.java
@@ -3,6 +3,7 @@ package com.driot.bookplayer.db;
 /**
  * created by Antoine Driot -- antoine.driot.com -- on 28/10/20
  */
+import androidx.room.AutoMigration;
 import androidx.room.Database;
 import androidx.room.RoomDatabase;
 import androidx.room.TypeConverters;
@@ -11,10 +12,13 @@ import androidx.room.TypeConverters;
                         Folder.class,
                         ZikFile.class
                      }
-                     , version = 3)
+                     , version = 3
+                    // on se fait les migrations a la main, en les ecrivant...... mais bon...
+                    //, autoMigrations = {@AutoMigration(from = 1, to = 2),@AutoMigration(from = 2, to = 3)}
+        )
 
-// v2 for field zeOrder
-// v3 for field displayName
+// v2 for field zeOrder ?????
+// v3 for field displayName -- 17 sept 2023
 
 @TypeConverters({Converters.class})
 
--- app/src/main/java/com/driot/bookplayer/db/DatabaseClient.java
+++ app/src/main/java/com/driot/bookplayer/db/DatabaseClient.java
@@ -4,13 +4,33 @@ package com.driot.bookplayer.db;
  * created by Antoine Driot -- antoine.driot.com -- on 28/10/20
  */
 
+import static com.driot.tonylib.KanLogger.myLog;
+
 import android.content.Context;
 
 import androidx.room.Room;
+import androidx.room.migration.Migration;
+import androidx.sqlite.db.SupportSQLiteDatabase;
 
 public class DatabaseClient {
 
     public static final String DATABASE_NAME = "BookPlayer";
+
+    static final Migration MIGRATION_1_2 = new Migration(1, 2) {
+        @Override
+        public void migrate(SupportSQLiteDatabase database) {
+            myLog("Migration -> executing step 1 => 2");
+            database.execSQL("ALTER TABLE ZikFile ADD COLUMN zeorder REAL NOT NULL default 0");
+        }
+    };
+
+    static final Migration MIGRATION_2_3 = new Migration(2, 3) {
+        @Override
+        public void migrate(SupportSQLiteDatabase database) {
+            myLog("Migration -> executing step 2 => 3");
+            database.execSQL("ALTER TABLE ZikFile ADD COLUMN displayName TEXT default '---'");
+            database.execSQL("UPDATE ZikFile SET displayName = Name");        }
+    };
 
     private Context mCtx;
     private static DatabaseClient mInstance;
@@ -32,7 +52,7 @@ public class DatabaseClient {
                 //                              modif version BDD => truncate all tables !!
                 //-------------------------------------------------------
 
-                .addMigrations(new MyMigration(1, 3))
+                .addMigrations(MIGRATION_1_2, MIGRATION_2_3)
                 .build();
 
     }
