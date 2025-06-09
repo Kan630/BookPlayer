f868cc8f8d312c293c502537f2d826054a854c6b
Antoine Driot <adriot@gmail.com>
2025-06-09 23:35:31 +0200

    v65 - traductions

--- app/src/main/java/com/driot/bookplayer/activities/MainActivity.java
+++ app/src/main/java/com/driot/bookplayer/activities/MainActivity.java
@@ -86,7 +86,7 @@ public class MainActivity extends LifecycleLoggingActivity {
 
         printSomeStuffAboutDevice();
 
-        Sql.log_all_Folders(this);
+        //Sql.log_all_Folders(this);
 
         setContentView(R.layout.activity_main);
 
