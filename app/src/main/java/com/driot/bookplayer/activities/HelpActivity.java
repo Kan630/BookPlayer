9fc0a06bdccbca3304df30790c3bf2f3c5402cdd
Antoine Driot <adriot@gmail.com>
2025-07-25 17:57:07 +0200

    manual page, some colors

--- app/src/main/java/com/driot/bookplayer/activities/HelpActivity.java
+++ app/src/main/java/com/driot/bookplayer/activities/HelpActivity.java
@@ -17,8 +17,38 @@ public class HelpActivity extends LoggingActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_help);
 
-        TextView tvHelpText = findViewById(R.id.tvHelpText);
-        tvHelpText.setText(Html.fromHtml(getString(R.string.help_text), Html.FROM_HTML_MODE_LEGACY));
+        TextView tv;
+
+        tv = findViewById(R.id.tvHelpText);
+        tv.setText(Html.fromHtml(getString(R.string.help_text_general), Html.FROM_HTML_MODE_LEGACY));
+
+        tv = findViewById(R.id.help_text_manual_import);
+        tv.setText(Html.fromHtml(getString(R.string.help_text_manual_import), Html.FROM_HTML_MODE_LEGACY));
+
+        tv = findViewById(R.id.help_librivox_text);
+        tv.setText(Html.fromHtml(getString(R.string.help_librivox_text), Html.FROM_HTML_MODE_LEGACY));
+
+        tv = findViewById(R.id.help_podcast_text);
+        tv.setText(Html.fromHtml(getString(R.string.help_podcast_text), Html.FROM_HTML_MODE_LEGACY));
+
+        tv = findViewById(R.id.help_url_text);
+        tv.setText(Html.fromHtml(getString(R.string.help_url_text), Html.FROM_HTML_MODE_LEGACY));
+
+
+        tv = findViewById(R.id.help_memory_cleaning_text);
+        tv.setText(Html.fromHtml(getString(R.string.help_memory_cleaning_text), Html.FROM_HTML_MODE_LEGACY));
+
+        tv = findViewById(R.id.help_storage_text);
+        tv.setText(Html.fromHtml(getString(R.string.help_storage_text), Html.FROM_HTML_MODE_LEGACY));
+
+        tv = findViewById(R.id.help_permission_text);
+        tv.setText(Html.fromHtml(getString(R.string.help_permission_text), Html.FROM_HTML_MODE_LEGACY));
+
+        tv = findViewById(R.id.help_tellme_text);
+        tv.setText(Html.fromHtml(getString(R.string.help_tellme_text), Html.FROM_HTML_MODE_LEGACY));
+
+        tv = findViewById(R.id.help_forum_text);
+        tv.setText(Html.fromHtml(getString(R.string.help_forum_text), Html.FROM_HTML_MODE_LEGACY));
 
     }
 }
