aeb89fce91ff3ae196b845a8aa58bd4a2d9656d3
Antoine Driot <adriot@gmail.com>
2025-10-11 14:53:55 +0200

    git add

---
+++ run-tests_sav01.bat
@@ -0,0 +1,40 @@
+@echo off
+echo ------------------------------------------------------------------------------
+echo ------------------------------------------------------------------------------
+echo Cleaning test state...
+adb uninstall com.driot.bookplayer 2>nul
+adb uninstall com.driot.bookplayer.test 2>nul
+
+echo ------------------------------------------------------------------------------
+echo ------------------------------------------------------------------------------
+echo Building APKs...
+call gradlew.bat :app:assembleDebug :app:assembleDebugAndroidTest
+if errorlevel 1 (
+    echo Build failed!
+    exit /b 1
+)
+
+echo ------------------------------------------------------------------------------
+echo ------------------------------------------------------------------------------
+echo Installing APKs...
+call gradlew.bat :app:installDebug :app:installDebugAndroidTest
+if errorlevel 1 (
+    echo Install failed!
+    exit /b 1
+)
+
+echo ------------------------------------------------------------------------------
+echo ------------------------------------------------------------------------------
+echo hard boot adb
+call C:\Users\adrio\AppData\Local\Android\Sdk\platform-tools\adb.exe shell am instrument -w -r -e class com.driot.bookplayer.test.JustOpenAndWait com.driot.bookplayer.test/androidx.test.runner.AndroidJUnitRunner
+
+echo ------------------------------------------------------------------------------
+echo ------------------------------------------------------------------------------
+echo hard boot adb 2
+call C:\Users\adrio\AppData\Local\Android\Sdk\platform-tools\adb.exe shell am instrument -w -r -e class com.driot.bookplayer.test.JustOpenAndWait com.driot.bookplayer.test/androidx.test.runner.AndroidJUnitRunner
+
+echo ------------------------------------------------------------------------------
+echo ------------------------------------------------------------------------------
+echo Running tests...
+call gradlew.bat :app:connectedDebugAndroidTest
+
