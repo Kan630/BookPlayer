0e1ffd72ee74f0e35071a7970e827116f5c57229
Antoine Driot <adriot@gmail.com>
2025-11-24 16:38:18 +0100

    Analytics trim

--- app/src/main/java/com/driot/bookplayer/helpers/FirebaseAnalyticsHelper.java
+++ app/src/main/java/com/driot/bookplayer/helpers/FirebaseAnalyticsHelper.java
@@ -36,12 +36,12 @@ public final class FirebaseAnalyticsHelper {
 
     public static void setCustomKeyCrashlytics(String strKey, String strValue) {
         myLogD("setCustomKeyCrashlytics : " + strKey + " = " + strValue);
-        FirebaseCrashlytics.getInstance().setCustomKey(strKey, String.valueOf(strValue));
+        FirebaseCrashlytics.getInstance().setCustomKey(strKey, trimFA(strValue));
     }
 
     public static void logCrashlytics(String strLog) {
         myLogD("logCrashlytics : " + strLog);
-        FirebaseCrashlytics.getInstance().log(String.valueOf(strLog));
+        FirebaseCrashlytics.getInstance().log(trimFA(strLog));
     }
 
 //ANALYTICS
@@ -50,7 +50,7 @@ public final class FirebaseAnalyticsHelper {
         try {
             myLogD("Analytics logging - " + event);
             FirebaseAnalytics firebaseAnalytics = FirebaseAnalytics.getInstance(appContext);
-            firebaseAnalytics.logEvent(String.valueOf(event), null);
+            firebaseAnalytics.logEvent(trimFA(event), null);
         } catch (Exception e) {
             myLogEE(e, "Analytics logging - " + event);
         }
@@ -60,16 +60,16 @@ public final class FirebaseAnalyticsHelper {
 
     public static void tellPlayFor1min(String elapsed_category, String playMode, String extension) {
         Bundle bundle = new Bundle();
-        bundle.putString("elapsed_category", String.valueOf(elapsed_category));
-        bundle.putString("extension", String.valueOf(extension));
+        bundle.putString("elapsed_category", trimFA(elapsed_category));
+        bundle.putString("extension", trimFA(extension));
         logBundleEvent( playMode + "_for_1min", bundle);
-        bundle.putString("play_mode", String.valueOf(playMode));
+        bundle.putString("play_mode", trimFA(playMode));
         logBundleEvent("play_for_1min", bundle);
     }
 
     public static void tellDbKo(String err_type, int nbFatalError, int nbInvalid, int nbZikFiles, int nbRewritten, int nbStillBad, String masterMsg) {
         Bundle bundle = new Bundle();
-        bundle.putString("err_type", String.valueOf(err_type));
+        bundle.putString("err_type", trimFA(err_type));
         bundle.putString("ei_invalid", nbInvalid + "/" + nbZikFiles);
         bundle.putString("ei_fatal", nbFatalError + "/" + nbZikFiles);
         bundle.putString("ei_rewritten_inv", nbRewritten + "/" + nbInvalid + "/" + nbZikFiles);
@@ -91,14 +91,14 @@ public final class FirebaseAnalyticsHelper {
     }
     private static Bundle getImportJobBundle(ImportJob j) {
         Bundle bundle = new Bundle();
-        bundle.putString("originalUri", String.valueOf(j.originalUri));
-        bundle.putString("taskName", String.valueOf(j.currentOperation));
-        bundle.putString("progressText", String.valueOf(j.progressText));
+        bundle.putString("originalUri", trimFA(j.originalUri));
+        bundle.putString("taskName", trimFA(j.currentOperation));
+        bundle.putString("progressText", trimFA(j.progressText));
         bundle.putBoolean("doDownload", j.doDownload);
-        bundle.putString("extension", String.valueOf(j.fileExtension));
-        bundle.putString("folderName", String.valueOf(j.futureFolderName));
-        bundle.putString("errorText", String.valueOf(j.errorTextDev));
-        bundle.putString("warningText", String.valueOf(j.warningText));
+        bundle.putString("extension", trimFA(j.fileExtension));
+        bundle.putString("folderName", trimFA(j.futureFolderName));
+        bundle.putString("errorText", trimFA(j.errorTextDev));
+        bundle.putString("warningText", trimFA(j.warningText));
         return bundle;
     }
 
@@ -106,10 +106,10 @@ public final class FirebaseAnalyticsHelper {
 
     public static void tellAnalyticsLoadFileKO(String filePath, String playMode) {
         Bundle bundle = new Bundle();
-        bundle.putString("playMode", String.valueOf(playMode));
-        bundle.putString("filePath", String.valueOf(filePath));
-        bundle.putString("fileName", Tonio.getFileNameFromPath(String.valueOf(filePath)));
-        bundle.putString("extension", Tonio.getExtension(String.valueOf(filePath)));
+        bundle.putString("playMode", trimFA(playMode));
+        bundle.putString("filePath", trimFA(filePath));
+        bundle.putString("fileName", Tonio.getFileNameFromPath(trimFA(filePath)));
+        bundle.putString("extension", Tonio.getExtension(trimFA(filePath)));
         logBundleEvent("load_file_KO", bundle);
     }
 
@@ -133,7 +133,7 @@ public final class FirebaseAnalyticsHelper {
 
     public static void tellCarSendCmd(String action) {
         Bundle bundle = new Bundle();
-        bundle.putString("action", String.valueOf(action));
+        bundle.putString("action", trimFA(action));
         logBundleEvent("car_send_cmd", bundle);
     }
 
@@ -141,132 +141,132 @@ public final class FirebaseAnalyticsHelper {
     public static void tellAnalyticsPlaylistLoadFromStorage(Context context, String storageType, String playlist) {
         Bundle bundle = new Bundle();
         bundle.putString("context", context.getClass().getSimpleName());
-        bundle.putString("storageType", String.valueOf(storageType));
-        bundle.putString("playlist", String.valueOf(playlist));
+        bundle.putString("storageType", trimFA(storageType));
+        bundle.putString("playlist", trimFA(playlist));
         logBundleEvent("playlist_load_from_storage", bundle);
     }
 
     public static void tellAnalyticsWork(LoadBookTaskState s, boolean doDownload) {
         Bundle bundle = new Bundle();
-        bundle.putString("originalUri", String.valueOf(s.originalUri));
-        bundle.putString("originalFile", String.valueOf(s.originalFile));
-        bundle.putString("extension", String.valueOf(s.fileExtension));
+        bundle.putString("originalUri", trimFA(String.valueOf(s.originalUri)));
+        bundle.putString("originalFile", trimFA(s.originalFile));
+        bundle.putString("extension", trimFA(s.fileExtension));
         bundle.putString("addToExistingFolder", String.valueOf (s.addToExistingFolderId>0));
-        bundle.putString("folderName", String.valueOf (s.futureFolderName));
-        bundle.putString("playType", String.valueOf (s.playType));
-        bundle.putString("sourceLocation", String.valueOf(s.sourceLocation));
-        bundle.putString("title", String.valueOf(s.title));
+        bundle.putString("folderName", trimFA (s.futureFolderName));
+        bundle.putString("playType", trimFA (s.playType));
+        bundle.putString("sourceLocation", trimFA(s.sourceLocation));
+        bundle.putString("title", trimFA(s.title));
         bundle.putBoolean("doDownload", doDownload);
         logBundleEvent("worker_start", bundle);
     }
     public static void tellAnalyticsManualLoad(String type, String extension, String sourceLocation, String originalFile) {
         Bundle bundle = new Bundle();
-        bundle.putString("type", String.valueOf(type));
-        bundle.putString("extension", String.valueOf(extension));
-        bundle.putString("sourceLocation", String.valueOf(sourceLocation));
-        bundle.putString("originalFile", String.valueOf(originalFile));
+        bundle.putString("type", trimFA(type));
+        bundle.putString("extension", trimFA(extension));
+        bundle.putString("sourceLocation", trimFA(sourceLocation));
+        bundle.putString("originalFile", trimFA(originalFile));
         logBundleEvent("manual_load", bundle);
     }
     public static void tellAnalyticsProxyLoad(String originalUri, String mode, boolean persistedPermission) {
         Bundle bundle = new Bundle();
-        bundle.putString("originalUri", String.valueOf(originalUri));
-        bundle.putString("mode", String.valueOf(mode));
+        bundle.putString("originalUri", trimFA(originalUri));
+        bundle.putString("mode", trimFA(mode));
         bundle.putString("persistedPermission", String.valueOf(persistedPermission));
         logBundleEvent("proxy_load", bundle);
     }
     public static void tellAnalyticsManualDownload(String fileUrl, String destinationFolder) {
         Bundle bundle = new Bundle();
-        bundle.putString("fileUrl", String.valueOf(fileUrl));
-        bundle.putString("fileName", Tonio.getFileNameFromUrl(String.valueOf(fileUrl)));
+        bundle.putString("fileUrl", trimFA(fileUrl));
+        bundle.putString("fileName", trimFA(Tonio.getFileNameFromUrl(String.valueOf(fileUrl))));
         bundle.putString("destinationFolder", String.valueOf(destinationFolder));
         logBundleEvent("just_get_it", bundle);
     }
 
     public static void tellEbookDownloadFromGutendex(String title) {
         Bundle bundle = new Bundle();
-        bundle.putString("title", String.valueOf(title));
+        bundle.putString("title", trimFA(title));
         logBundleEvent("Gutendex_download", bundle);
     }
 
     public static void tellLibrivoxDownload(String title) {
         Bundle bundle = new Bundle();
-        bundle.putString("title", String.valueOf(title));
+        bundle.putString("title", trimFA(title));
         logBundleEvent("librivox_download", bundle);
     }
     public static void tellLibrivoxSuccess(String title) {
         Bundle bundle = new Bundle();
-        bundle.putString("title", String.valueOf(title));
+        bundle.putString("title", trimFA(title));
         logBundleEvent("librivox_success", bundle);
     }
 
     public static void tellAnalyticsLibrivoxSearch(String query, String lang) {
         Bundle bundle = new Bundle();
-        bundle.putString("query", String.valueOf(query));
-        bundle.putString("language", String.valueOf(lang));
+        bundle.putString("query", trimFA(query));
+        bundle.putString("language", trimFA(lang));
         logBundleEvent("librivox_search", bundle);
     }
     public static void  tellAnalyticsLibrivoxTrending(String query, String lang) {
         Bundle bundle = new Bundle();
-        bundle.putString("query", String.valueOf(query));
-        bundle.putString("language", String.valueOf(lang));
+        bundle.putString("query", trimFA(query));
+        bundle.putString("language", trimFA(lang));
         logBundleEvent("librivox_trending", bundle);
     }
 
     public static void tellAnalyticsPodcastSearch(String query, String lang) {
         Bundle bundle = new Bundle();
-        bundle.putString("query", String.valueOf(query));
-        bundle.putString("language", String.valueOf(lang));
+        bundle.putString("query", trimFA(query));
+        bundle.putString("language", trimFA(lang));
         logBundleEvent("podcast_search", bundle);
     }
     public static void tellAnalyticsPodcastTrending(String query, String lang) {
         Bundle bundle = new Bundle();
-        bundle.putString("query", String.valueOf(query));
-        bundle.putString("language", String.valueOf(lang));
+        bundle.putString("query", trimFA(query));
+        bundle.putString("language", trimFA(lang));
         logBundleEvent("podcast_trending", bundle);
     }
     public static void tellAnalyticsPodcastFavorite(String podcastName, String lang) {
         Bundle bundle = new Bundle();
-        bundle.putString("podcastName", String.valueOf(podcastName));
-        bundle.putString("language", String.valueOf(lang));
+        bundle.putString("podcastName", trimFA(podcastName));
+        bundle.putString("language", trimFA(lang));
         logBundleEvent("podcast_favorite", bundle);
     }
     public static void tellAnalyticsPodcastAutoDownload(String podcastName, String lang) {
         Bundle bundle = new Bundle();
-        bundle.putString("podcastName", String.valueOf(podcastName));
-        bundle.putString("language", String.valueOf(lang));
+        bundle.putString("podcastName", trimFA(podcastName));
+        bundle.putString("language", trimFA(lang));
         logBundleEvent("podcast_autodownload", bundle);
     }
     public static void tellAnalyticsPodcastRefresh(String podcast_title) {
         Bundle bundle = new Bundle();
-        bundle.putString("podcast_title", String.valueOf(podcast_title));
+        bundle.putString("podcast_title", trimFA(podcast_title));
         logBundleEvent("podcast_refresh", bundle);
     }
 
     public static void tellAnalyticsPlayAction(String actionName, String folderName) {
         Bundle bundle = new Bundle();
-        bundle.putString("actionName", String.valueOf(actionName));
-        bundle.putString("folderName", String.valueOf(folderName));
+        bundle.putString("actionName", trimFA(actionName));
+        bundle.putString("folderName", trimFA(folderName));
         logBundleEvent("play_action", bundle);
     }
     public static void tellAnalyticsEbookWorker(String extension) {
         Bundle bundle = new Bundle();
-        bundle.putString("extension", String.valueOf(extension));
+        bundle.putString("extension", trimFA(extension));
         logBundleEvent("ebook_worker", bundle);
     }
     public static void tellAnalyticsStartStreaming(String podcastName, String stream_url, String play_mode) {
         Bundle bundle = new Bundle();
-        bundle.putString("podcastName", String.valueOf(podcastName));
-        bundle.putString("stream_url", String.valueOf(stream_url));
-        bundle.putString("play_mode", String.valueOf(play_mode));
+        bundle.putString("podcastName", trimFA(podcastName));
+        bundle.putString("stream_url", trimFA(stream_url));
+        bundle.putString("play_mode", trimFA(play_mode));
         logBundleEvent("start_streaming", bundle);
     }
 
     public static void tellAnalyticsRadioTrending(String query, String lang, String country, String tag) {
         Bundle bundle = new Bundle();
-        bundle.putString("query", String.valueOf(query));
-        bundle.putString("language", String.valueOf(lang));
-        bundle.putString("country", String.valueOf(country));
-        bundle.putString("tag", String.valueOf(tag));
+        bundle.putString("query", trimFA(query));
+        bundle.putString("language", trimFA(lang));
+        bundle.putString("country", trimFA(country));
+        bundle.putString("tag", trimFA(tag));
         logBundleEvent("radio_trending", bundle);
     }
     public static void tellAnalyticsRadioBy(String byWhat) {
@@ -276,16 +276,16 @@ public final class FirebaseAnalyticsHelper {
 
     public static void tellAnalyticsRadioSearch(String query, String lang, String country, String tag) {
         Bundle bundle = new Bundle();
-        bundle.putString("query", String.valueOf(query));
-        bundle.putString("language", String.valueOf(lang));
-        bundle.putString("country", String.valueOf(country));
-        bundle.putString("tag", String.valueOf(tag));
+        bundle.putString("query", trimFA(query));
+        bundle.putString("language", trimFA(lang));
+        bundle.putString("country", trimFA(country));
+        bundle.putString("tag", trimFA(tag));
         logBundleEvent("radio_search", bundle);
     }
 
     public static void tellAnalyticsLogee(String customErrorTxt, String androidErrorText, String from) {
         Bundle bundle = new Bundle();
-        bundle.putString("from", String.valueOf(from));
+        bundle.putString("from", trimFA(from));
         bundle.putString("customErrorTxt", trimFA(customErrorTxt));
         bundle.putString("androidErrorText", trimFA(androidErrorText));
         logBundleEvent("log_ee", bundle);
