9d4c4a2573c07dd0a0ac135a6306b7dedd9e4678
Antoine Driot <adriot@gmail.com>
2025-12-04 14:42:40 +0100

    livbrivox facets(genres) clean

--- app/src/main/java/com/driot/bookplayer/librivox/GetLibrivoxFacetListActivity.java
+++ app/src/main/java/com/driot/bookplayer/librivox/GetLibrivoxFacetListActivity.java
@@ -6,8 +6,6 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ProgressBar;
 
-import androidx.annotation.IntDef;
-import androidx.annotation.Nullable;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -19,14 +17,8 @@ import com.driot.bookplayer.global.Var;
 import com.driot.bookplayer.helpers.InsetHelper;
 import com.driot.bookplayer.helpers.ViewHelper;
 
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
 import java.util.ArrayList;
 import java.util.List;
-
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
 
 public class GetLibrivoxFacetListActivity extends BaseBottomNavActivity {
 
@@ -34,10 +26,6 @@ public class GetLibrivoxFacetListActivity extends BaseBottomNavActivity {
 
     public static final int MODE_GENRE  = 0;
     public static final int MODE_AUTHOR = 1; // we’ll wire this later
-
-    @IntDef({MODE_GENRE, MODE_AUTHOR})
-    @Retention(RetentionPolicy.SOURCE)
-    public @interface FacetMode {}
 
     private RecyclerView rv;
     private ProgressBar progress;
@@ -63,72 +51,31 @@ public class GetLibrivoxFacetListActivity extends BaseBottomNavActivity {
     @Override
     protected void onCreate(Bundle b) {
         super.onCreate(b);
+        InsetHelper.apply(this);
 
         rv = findViewById(R.id.recyclerView);
         progress = findViewById(R.id.progressBar);
 
         librivoxLanguageItem = (LibrivoxLanguageItem) getIntent().getSerializableExtra(Intents.EXTRA_LIBRIVOX_LANGUAGE_ITEM);
 
-        int span = getResources().getInteger(R.integer.classic_grid_span);
-        if (span < 2) span = 2;
-        GridLayoutManager glm = new GridLayoutManager(this, span);
-        rv.setLayoutManager(glm);
-        rv.addItemDecoration(
-                new ViewHelper.SpacesItemDecoration(ViewHelper.dp(this, Var.GRID_LAYOUT_SPACER))
-        );
-
+        if (rv != null) {
+            int span = getResources().getInteger(R.integer.classic_grid_span);
+            GridLayoutManager glm = new GridLayoutManager(this, span);
+            rv.setLayoutManager(glm);
+            rv.setHasFixedSize(true);
+            rv.addItemDecoration(new ViewHelper.SpacesItemDecoration(ViewHelper.dp(this, Var.GRID_LAYOUT_SPACER)));
+        }
         adapter = new LibrivoxFacetCardAdapter(item -> {
-            // User clicked a ROOT GENRE → open subgenre screen
             myLogI("--- user clicks genre " + item.name);
             openLibrivoxResultsForGenre(item.name);
         });
-
         rv.setAdapter(adapter);
 
-        loadRootGenres();
+        loadGenres();
 
     }
 
-
-    private void loadFacetItems(@FacetMode int mode) {
-        progress.setVisibility(View.VISIBLE);
-
-        switch (mode) {
-            case MODE_AUTHOR:
-                // TODO next step: listTopAuthors(lang, …)
-                // For now, just show empty list
-                progress.setVisibility(View.GONE);
-                adapter.setItems(new ArrayList<>());
-                myLogW("GetLibrivoxFacetListActivity: MODE_AUTHOR not implemented yet");
-                break;
-
-            case MODE_GENRE:
-            default:
-                repo.listTopGenres(librivoxLanguageItem.code3, Var.LIBRIVOX_LIST_MAX_CARD_ITEM, new Callback<>() {
-                    @Override
-                    public void onResponse(Call<List<LibrivoxFacetItem>> call,
-                                           Response<List<LibrivoxFacetItem>> rsp) {
-                        progress.setVisibility(View.GONE);
-                        if (rsp.isSuccessful() && rsp.body() != null) {
-                            adapter.setItems(rsp.body());
-                        } else {
-                            adapter.setItems(new ArrayList<>());
-                            myLogEE(null, "listTopGenres empty/invalid response");
-                        }
-                    }
-
-                    @Override
-                    public void onFailure(Call<List<LibrivoxFacetItem>> call, Throwable t) {
-                        progress.setVisibility(View.GONE);
-                        adapter.setItems(new ArrayList<>());
-                        myLogEE(t, "listTopGenres failed");
-                    }
-                });
-                break;
-        }
-    }
-
-    private void loadRootGenres() {
+    private void loadGenres() {
         progress.setVisibility(View.VISIBLE);
         List<LibrivoxGenre> all = LibrivoxGenreStore.getGenres(this);
         List<LibrivoxFacetItem> out = new ArrayList<>();
