4276169b8fd42c74a7bb305d3608648eef300a8e
Antoine Driot <adriot@gmail.com>
2026-02-28 17:34:07 +0100

    gutenberg caching (cloudfare)

--- app/src/main/java/com/driot/bookplayer/ebooks/gutendex/GutendexApiService.java
+++ app/src/main/java/com/driot/bookplayer/ebooks/gutendex/GutendexApiService.java
@@ -1,6 +1,8 @@
 // GutendexApiService.java
 package com.driot.bookplayer.ebooks.gutendex;
 
+import com.driot.bookplayer.BuildConfig;
+
 import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Query;
@@ -8,7 +10,8 @@ import retrofit2.http.Url;
 
 public interface GutendexApiService {
 
-    String BASE_URL = "https://gutendex.com/";
+    //String BASE_URL = "https://gutendex.com/";
+    String BASE_URL = BuildConfig.GUTENDEX_BASE_URL;
 
     // Main search endpoint: /books?search=...&languages=en,fr&page=1
     @GET("books")
