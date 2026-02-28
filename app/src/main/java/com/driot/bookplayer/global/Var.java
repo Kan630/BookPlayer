2a2a218a9a3770353d959ee58cc5980188757f98
Antoine Driot <adriot@gmail.com>
2026-02-28 18:18:36 +0100

    gutenberg

--- app/src/main/java/com/driot/bookplayer/global/Var.java
+++ app/src/main/java/com/driot/bookplayer/global/Var.java
@@ -25,7 +25,7 @@ public class Var {
         public static final int RADIO_STATION_MAX_DUPLICATES = 3; // change value in strings...
         public static final int RADIO_BROWSER_TIMEOUT_SEC = 10;
         public static final int GUTENDEX_CONNECT_TIMEOUT_SEC = 10;
-        public static final int GUTENDEX_READ_TIMEOUT_SEC = 30;
+        public static final int GUTENDEX_READ_TIMEOUT_SEC = 90;
 
         /**
          * Station names to hide when "remove dubious" is enabled (e.g. added to every
